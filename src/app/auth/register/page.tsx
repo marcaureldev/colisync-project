7396a8a1a8c1493d1158753fd97b4a90ed4cb078
Reqima-dev <adloukondo@gmail.com>
2025-05-13 11:48:02 +0100

    Adds updatedAt fields and user registration endpoint
    Introduces `updatedAt` fields with automatic timestamp updates to User and Auth models.
    Implements user registration endpoint with email verification logic, including OTP and token generation.
    Updates dependencies: adds `react-spinners` and `uuid`.
    Removes unused registration page component.
    
    Enhances database schema and improves user onboarding workflow.

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,48 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  useEffect(() => {
+    if (password !== confirmPassword) {
+      setError("Les mots de passe ne correspondent pas");
+    } else {
+      setError("");
+    }
+  }, [password, confirmPassword]);
+  const onSubmit = async () => {
+    if (!name.trim() || !email.trim() || !password.trim()) {
+      setError("Veuillez remplir tous les champs");
+      return;
+    }
+    const data = {
+      email,
+      name,
+      password,
+    };
+    try {
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      if (result.success === false) {
+        setError(result.error);
+      }
+      router.push(result.redirectUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <section className="h-screen">
-      {/* <div className="bg-[url(/images/register.svg)] bg-no-repeat bg-cover bg- h-full w-1/2"></div> */}
       <div className="flex flex-col items-center justify-center h-full bg-gradient-to-r from-[#0F123B] via-[#090D2E] to-[rgb(2,5,21)] ">
         <div className="flex flex-col items-center justify-center text-white text-center border p-8 rounded-lg shadow-lg bg-white/5 backdrop-blur-lg border-white/10 h-[550px] w-[400px]">
           <h1 className="text-3xl text-white font-bold">Créer un compte</h1>
@@ -15,26 +53,33 @@ const register = () => {
             <input
               type="email"
               placeholder="Entrez votre adresse email"
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full p-2 bg-white/5 backdrop-blur-lg rounded-lg border border-white/10"
             />
             <input
               type="text"
               placeholder="Entrez votre nom et prénom"
+              onChange={(e) => setName(e.target.value)}
               className="w-full p-2 bg-white/5 backdrop-blur-lg rounded-lg border border-white/10"
             />
             <input
               type="password"
               placeholder="Entrez un mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full p-2 bg-white/5 backdrop-blur-lg rounded-lg border border-white/10"
             />
             <input
               type="password"
               placeholder="Confirmez le mot de passe"
+              onChange={(e) => setConfirmPassword(e.target.value)}
               className="w-full p-2 bg-white/5 backdrop-blur-lg rounded-lg border border-white/10"
             />
+            {error && <p className="text-red-500 text-xs">{error}</p>}
+          </form>
+          <div className="mt-2.5">
             <button
-              type="submit"
-              className="w-full p-2 bg-blue-500 text-white rounded-lg"
+              onClick={onSubmit}
+              className="w-full p-2 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-400"
             >
               S'inscrire
             </button>
@@ -43,7 +88,7 @@ const register = () => {
               Vous avez déjà un compte?{" "}
               <Link href="/login">Connectez-vous</Link>
             </p>
-          </form>
+          </div>
         </div>
       </div>
     </section>
